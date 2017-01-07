ed074660c6e8927c02d475eb614bd86421aa7dcf
Charles Weng <cweng@stuy.edu>
2017-01-07 02:02:44 -0500

    basic template for RPG Game and Unit

--- Game.java
+++ Game.java
@@ -1,2 +1,28 @@
 public class Game{
+    private static Map map;
+    private static Enemy[] enemies;
+    private static Character player;
+
+    //default constructors
+    public Game(){
+	map = new Map();
+	player = new Character();
+    }
+
+    //checks if the player is alive or not
+    public boolean getAlive(){
+	return player.getAlive();
+    }
+
+    //not finished 
+    public void interpret(String command){
+    }
+
+
+    //should print out all the nessecary information
+    public String toString(){
+	String printOut = map.toString();
+	printOut += "/n/n";
+	return printOut;
+    }
 }

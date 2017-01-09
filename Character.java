4f15d6023123b18078cafa51f9e04570ce75b85c
Charles Weng School <cweng@stuy.edu>
2017-01-09 15:17:02 -0500

    fixed up character

--- Character.java
+++ Character.java
@@ -1,17 +1,25 @@
 public class Character extends Unit{
-	//public Inventory inv = new Inventory();
-	public double hp = 100.00;
-	public double mp = 100.00;
-    public int atk = 10;
-	public int def = 0;
-	public int dex = 10;
-	public int vit = 10;
-	public int wis = 10;
-	//weapon = inv.getWeapon();
-	public int xcor = 1;
-	public int ycor = 1;
+    public double hp,mp;
+    public int atk, def, dex, vit, wis, xcor, ycor;
+    public String weapon;
+    //public Inventory inv;
 
+    public Character{
+	hp = 100.00;
+	mp = 100.00;
+	atk = 10;
+	def = 0;
+	dex = 10;
+	vit = 10;
+	wis = 10;
+	//weapon = inv.getWeapon();
+	xcor = 1;
+	ycor = 1;
+	//inv = new Inventory();
+    }
+    
     public void attack(){}
 
-    public void move(){}
+    public void move(){
+    }
 }

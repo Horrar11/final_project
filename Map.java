89ddc5338fb3556e1a55c39a23b39b7bac1765f1
Charles Weng <cweng@stuy.edu>
2017-01-10 03:12:44 -0500

    added in comments

--- Map.java
+++ Map.java
@@ -1,10 +1,12 @@
+import java.util.Random;
+
 public class Map{
     public char[][] grid;
-    private int playerX = 1;
-    private int playerY = 1;
-    private char saveChar = ' ';
+    private int playerX;
+    private int playerY;
+    private char saveChar = ' ';//don't know why anton has this
 
-    
+    //sets up the border of the map and randomly places the player
     public Map(int row, int col){
 	grid = new char[row][col];
 	for(int i = 0;i < row;i++){
@@ -15,9 +17,13 @@ public class Map{
 		}
 	    }
 	}
+	Random rand = new Random();
+	playerX = rand.nextInt(row - 2) + 1;
+	playerY = rand.nextInt(col - 2) + 1; 
 	setPlayerPos();
     }
-    
+
+    //puts a 'P' on the map wherever the player is
     public void setPlayerPos(){grid[playerX][playerY] = 'P';}
 	
     public void setPXY(int x, int y){playerX = x; playerY = y;}
@@ -33,16 +39,20 @@ public class Map{
 	}
 	return toRet;
     }
-	
+
+    //generic error for invalid movements to make code neater
     public void mOOB(String input) throws ArrayIndexOutOfBoundsException{
 	throw new ArrayIndexOutOfBoundsException(input);
     }
 
-    //for replacing the old p
-    public void makeLand(int x, int y){grid[x][y] = saveChar;}
+    //for replacing the old 'P's with ' ' 
+    public void makeLand(int x, int y){grid[x][y] = ' ';}
     //No idea what anton wanted this to be
     public void restoreLand(int x, int y){saveChar = grid[x][y];}
-	
+
+
+    //determines how the the commands it gets from Game affects the grid
+    //returns false if movement is invalid in case that's usefull
     public boolean interpret(String arg){
 	try{
 	switch(arg){

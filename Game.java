1af4ce14c452dd8831b8362fabddb5941a7df5b3
Slav Man <anthony.horrar11@yahoo.com>
2017-01-13 15:40:44 -0500

    Freaking merge issues man!

--- Game.java
+++ Game.java
@@ -76,67 +76,41 @@ public class Game{
 	spawnEnemies();
 	map.setPlayerPos();
     }
-
-    <<<<<<< HEAD
-	    //clears the screen
-	private static void clearScreen(){
-	System.out.print("\033[2J\033[;H");
+    //checks if the player is alive or not
+    public boolean getAlive(){
+	return player.getAlive();
     }
     
-    //heart of the game
-    private static void game(){
-	System.out.println(map.display());
-	System.out.print("\tCommand:");
-	Scanner console = new Scanner(System.in);
-	String command = console.nextLine();
-	clearScreen();
-	map.interpret(command);
+    //sends command over to map to see if the map can place
+    //the player in the place he wants to go, if not then 
+    //nothing happens and the player recieves a
+    //"You can't go there!" message.
+    public void interpret(String command){
+	String given = command;
+	switch(given){
+	case "w": if (map.interpret(command)){player.xcor--;} break;
+	case "a": if (map.interpret(command)){player.ycor--;} break;
+	case "s": if (map.interpret(command)){player.xcor++;} break;
+	case "d": if (map.interpret(command)){player.ycor++;} break;
+	case "yes": if(map.saveChar == 'S'){map = new Map(20,30); spawnEnemies();} break;
+	case "help": System.out.println(help);
+	default: System.out.println("404 command not found.\nTry typing help for\na list of available commands.");
+	}
     }
-
-    //checks whether or not 
-    public static boolean getAlive(){
-	return alive;
+    
+    //populate the enemies array
+    public void spawnEnemies(){
+	Random amount = new Random();
+	enemies = new Enemy[amount.nextInt(10)+1];
+	for (int i = 0; i < enemies.length; i++){
+	    enemies[i] = new Enemy(amount.nextInt(Math.abs(map.lengthX - 2) + 1),amount.nextInt(Math.abs(map.lengthY - 2) + 1));
+	}
     }
-
-    public static void setAlive(boolean val){
-	alive = val;
-	=======
-	    //checks if the player is alive or not
-	    public boolean getAlive(){
-		return player.getAlive();
-	    }
-	
-	    //sends command over to map to see if the map can place
-	    //the player in the place he wants to go, if not then 
-	    //nothing happens and the player recieves a
-	    //"You can't go there!" message.
-	    public void interpret(String command){
-		String given = command;
-		switch(given){
-		case "w": if (map.interpret(command)){player.xcor--;} break;
-		case "a": if (map.interpret(command)){player.ycor--;} break;
-		case "s": if (map.interpret(command)){player.xcor++;} break;
-		case "d": if (map.interpret(command)){player.ycor++;} break;
-		case "yes": if(map.saveChar == 'S'){map = new Map(20,30); spawnEnemies();} break;
-		case "help": System.out.println(help);
-		default: System.out.println("404 command not found.\nTry typing help for\na list of available commands.");
-		}
-	    }
-
-	    //populate the enemies array
-	    public void spawnEnemies(){
-		Random amount = new Random();
-		enemies = new Enemy[amount.nextInt(10)+1];
-		for (int i = 0; i < enemies.length; i++){
-		    enemies[i] = new Enemy(amount.nextInt(Math.abs(map.lengthX - 2) + 1),amount.nextInt(Math.abs(map.lengthY - 2) + 1));
-		}
-	    }
-	
-	    //should print out all the nessecary information
-	    public String toString(){
-		String printOut = map.toString();
-		printOut += "\n";
-		return printOut;
-		>>>>>>> 0fc85d0b884e9de2d598e135d14fa7be9d801ba9
-			    }
+    
+    //should print out all the nessecary information
+    public String toString(){
+	String printOut = map.toString();
+	printOut += "\n";
+	return printOut;
     }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# final_project
+<h1>LKBFCW's RPG</h1>
+<h2>Description:</h2>
+<p>Grid RPG, where you play to get a high score, by going deeper into the dungeon.</p><br>
+<h2>Working Features (Testable stuff):</h2>
+<p>Movement of the player and enemies - does not interfear with anything, meaning it cannot destory terrain, delete enemies by walking on them etc.</p><br>
+<p></p><br>
+<h2>Unresolved Bugs:</h2>
+<p>None for now, working on a lot of features though</p><br>
+<h2>How to Compile:</h2>
+<p>javac RPG.java</p><br>
+<h2>Directions how to use program:</h2>
+<p>java RPG</p><br>
+<p>Run the above for further instructions...</p><br>
